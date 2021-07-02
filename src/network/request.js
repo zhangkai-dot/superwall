18771e106e34c9851f59f2af17e0525c9aa5c5b9
zhangkai-dot <1216318121@qq.com>
2021-07-02 17:12:32 +0800

    初始化项目

---
+++ src/network/request.js
@@ -0,0 +1,47 @@
+import Axios from "axios";
+export function request(config) {
+  //1.创建axios实例
+  const instance = Axios.create({
+    baseURL: "http://123.207.32.32:8000",
+    timeout: 5000
+  });
+  //2.axios的拦截器
+  //2.1请求拦截
+  instance.interceptors.request.use(
+    config => {
+      return config;
+    },
+    err => {
+      // console.log(err);
+    }
+  );
+  //2.2响应拦截
+  instance.interceptors.response.use(
+    res => {
+      return res;
+      // console.log(res);
+    },
+    err => {
+      // console.log(err);
+    }
+  );
+
+  //3.发送真正的网络请求
+  return instance(config);
+}
+// export function request(config) {
+//   return new Promise((resolve, reject) => {
+//     //1.创建axios实例
+//     const instance = Axios.create({
+//       baseURL: "http://123.207.32.32:8000",
+//       timeout: 5000
+//     });
+//     instance(config)
+//       .then(res => {
+//         resolve(res);
+//       })
+//       .catch(err => {
+//         reject(err);
+//       });
+//   });
+// }
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,10 +1,43 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommends :recommends="recommends"></recommends>
+  </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import recommends from "./childComps/RecommendView.vue";
+import { getHomeMultidata } from "network/home";
+
+export default {
+  components: {
+    NavBar,
+    HomeSwiper,
+    recommends,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    //函数调用以后返回的是promise
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.data.banner.list;
+      this.recommends = res.data.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
